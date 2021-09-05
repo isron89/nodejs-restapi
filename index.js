b8aecae3316a448574792bc3b9ea94c0190e56aa
isron89 <isronid@gmail.com>
2021-09-05 15:04:09 +0700

    file http server

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require('http');
+ 
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+ 
+    response.statusCode = 200;
+    response.end('<h1>Halo Isron HTTP Server!</h1>');
+};
+ 
+ 
+const server = http.createServer(requestListener);
+ 
+const port = 5000;
+const host = 'localhost';
+ 
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
---
+++ server.js
@@ -0,0 +1,43 @@
+const http = require('http');
+ 
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    response.statusCode = 200;
+ 
+    const { method } = request;
+ 
+    if(method === 'GET') {
+        response.end('<h1>Hello!</h1>');
+    }
+ 
+    if(method === 'POST') {
+        let body = [];
+    
+      request.on('data', (chunk) => {
+          body.push(chunk);
+      });
+     
+      request.on('end', () => {
+          body = Buffer.concat(body).toString();
+          const { name, age } = JSON.parse(body);
+          response.end(`<h1>Hai, ${name}! umur : ${age}</h1>`);
+      });
+    }
+ 
+    if(method === 'PUT') {
+        response.end('<h1>Bonjour!</h1>');
+    }
+ 
+    if(method === 'DELETE') {
+        response.end('<h1>Salam!</h1>');
+    }
+};
+ 
+const server = http.createServer(requestListener);
+ 
+const port = 5000;
+const host = 'localhost';
+ 
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
